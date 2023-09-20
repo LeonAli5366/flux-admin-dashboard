13fdfac3e3edcc44b04bf753d0415520cb0ba5e9
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-09-20 16:12:03 -0600

    loading

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ import CreateStory from "./page/OurStory/CreateStory";
 import UpdateStory from "./page/OurStory/UpdateStory";
 import AllStory from "./page/OurStory/AllStory";
 import { AuthContext } from "./page/ContextApi/UserContex";
+import { ClockLoader } from "react-spinners";
 
 function App() {
-  const { user, adminLogin } = useContext(AuthContext);
+  const { user, adminLogin, loading } = useContext(AuthContext);
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
   const [error, setError] = useState("");
@@ -47,83 +48,99 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div>
-          {user?.uid ? (
-            <div className="app">
-              <Toaster position="top-center" reverseOrder={false} />
-              <Sidebar isSidebar={isSidebar} />
-              <main className="content">
-                <Topbar setIsSidebar={setIsSidebar} />
-                <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/invoices" element={<Invoices />} />
-                  <Route path="/form" element={<Form />} />
-
-                  {/* Product Route*/}
-                  <Route path="/allproduct" element={<AllProduct />} />
-                  <Route path="/createproduct" element={<CreateProduct />} />
-                  <Route
-                    path="/updateproduct/:slug"
-                    element={<UpdateProduct />}
-                  />
-
-                  {/* Our Story */}
-                  <Route path="/allstory" element={<AllStory />} />
-                  <Route path="/createstory" element={<CreateStory />} />
-                  <Route path="/updatestory/:slug" element={<UpdateStory />} />
-
-                  {/* Home Page All Route */}
-                  <Route path="/hometopsec" element={<HomeTopSec />} />
-                </Routes>
-              </main>
+          {loading ? (
+            <div className="flex justify-center pt-[20vh]">
+              <ClockLoader color="#36d7b7" size={500} />
             </div>
           ) : (
-            <div className="mx-auto">
-              <div className="hero min-h-screen ">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                  <div className="card flex-shrink-0 shadow-2xl bg-base-100 w-[500px]">
-                    <form
-                      onSubmit={handleAdminLogin}
-                      className="card-body text-[#141b2d]">
-                      <div className="form-control ">
-                        <h1 className="text-[#141b2d] text-5xl font-bold text-center mb-5">
-                          ADMIN LOGIN
-                        </h1>
-                        <label className="label">
-                          <span className="label-text">Email</span>
-                        </label>
-                        <input
-                          type="email"
-                          name="email"
-                          placeholder="email"
-                          className="input input-bordered"
+            <div>
+              <div>
+                {user?.uid ? (
+                  <div className="app">
+                    <Toaster position="top-center" reverseOrder={false} />
+                    <Sidebar isSidebar={isSidebar} />
+                    <main className="content">
+                      <Topbar setIsSidebar={setIsSidebar} />
+                      <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/invoices" element={<Invoices />} />
+                        <Route path="/form" element={<Form />} />
+
+                        {/* Product Route*/}
+                        <Route path="/allproduct" element={<AllProduct />} />
+                        <Route
+                          path="/createproduct"
+                          element={<CreateProduct />}
                         />
-                      </div>
-                      <div className="form-control">
-                        <label className="label">
-                          <span className="label-text">Password</span>
-                        </label>
-                        <input
-                          type="password"
-                          name="password"
-                          placeholder="password"
-                          className="input input-bordered"
+                        <Route
+                          path="/updateproduct/:slug"
+                          element={<UpdateProduct />}
                         />
-                        <label className="label">
-                          <Link
-                            href="#"
-                            className="label-text-alt link link-hover mt-1">
-                            Forgot password?
-                          </Link>
-                        </label>
-                      </div>
-                      <div className="form-control mt-6">
-                        <button type="submit" className="btn btn-primary">
-                          Login
-                        </button>
-                      </div>
-                    </form>
+
+                        {/* Our Story */}
+                        <Route path="/allstory" element={<AllStory />} />
+                        <Route path="/createstory" element={<CreateStory />} />
+                        <Route
+                          path="/updatestory/:slug"
+                          element={<UpdateStory />}
+                        />
+
+                        {/* Home Page All Route */}
+                        <Route path="/hometopsec" element={<HomeTopSec />} />
+                      </Routes>
+                    </main>
                   </div>
-                </div>
+                ) : (
+                  <div className="mx-auto">
+                    <div className="hero min-h-screen ">
+                      <div className="hero-content flex-col lg:flex-row-reverse">
+                        <div className="card flex-shrink-0 shadow-2xl bg-base-100 w-[500px]">
+                          <form
+                            onSubmit={handleAdminLogin}
+                            className="card-body text-[#141b2d]">
+                            <div className="form-control ">
+                              <h1 className="text-[#141b2d] text-5xl font-bold text-center mb-5">
+                                ADMIN LOGIN
+                              </h1>
+                              <label className="label">
+                                <span className="label-text">Email</span>
+                              </label>
+                              <input
+                                type="email"
+                                name="email"
+                                placeholder="email"
+                                className="input input-bordered"
+                              />
+                            </div>
+                            <div className="form-control">
+                              <label className="label">
+                                <span className="label-text">Password</span>
+                              </label>
+                              <input
+                                type="password"
+                                name="password"
+                                placeholder="password"
+                                className="input input-bordered"
+                              />
+                              <label className="label">
+                                <Link
+                                  href="#"
+                                  className="label-text-alt link link-hover mt-1">
+                                  Forgot password?
+                                </Link>
+                              </label>
+                            </div>
+                            <div className="form-control mt-6">
+                              <button type="submit" className="btn btn-primary">
+                                Login
+                              </button>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           )}
